ab85816b18de312bb778b2c0c6559697392f2b94
Your Name <thestackdev@gmail.com>
2024-04-22 21:32:44 +0700

    user design1_1

--- finaldesktop/UserMoney1_1.Designer.cs
+++ finaldesktop/UserMoney1_1.Designer.cs
@@ -28,19 +28,310 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.txtfamily = new System.Windows.Forms.TextBox();
+            this.idfamily = new System.Windows.Forms.Label();
+            this.txtphone = new System.Windows.Forms.TextBox();
+            this.txtfullname = new System.Windows.Forms.TextBox();
+            this.txtuser = new System.Windows.Forms.TextBox();
+            this.idfullname = new System.Windows.Forms.Label();
+            this.idphone = new System.Windows.Forms.Label();
+            this.idciz = new System.Windows.Forms.Label();
+            this.iduser = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.txtsel = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // txtfamily
+            // 
+            this.txtfamily.Location = new System.Drawing.Point(453, 200);
+            this.txtfamily.Name = "txtfamily";
+            this.txtfamily.Size = new System.Drawing.Size(235, 20);
+            this.txtfamily.TabIndex = 50;
+            // 
+            // idfamily
+            // 
+            this.idfamily.AutoSize = true;
+            this.idfamily.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.idfamily.Location = new System.Drawing.Point(241, 200);
+            this.idfamily.Name = "idfamily";
+            this.idfamily.Size = new System.Drawing.Size(86, 18);
+            this.idfamily.TabIndex = 49;
+            this.idfamily.Text = "รหัสครอบครัว";
+            // 
+            // txtphone
+            // 
+            this.txtphone.Location = new System.Drawing.Point(451, 298);
+            this.txtphone.Name = "txtphone";
+            this.txtphone.Size = new System.Drawing.Size(235, 20);
+            this.txtphone.TabIndex = 48;
+            // 
+            // txtfullname
+            // 
+            this.txtfullname.Location = new System.Drawing.Point(453, 347);
+            this.txtfullname.Name = "txtfullname";
+            this.txtfullname.Size = new System.Drawing.Size(235, 20);
+            this.txtfullname.TabIndex = 47;
+            // 
+            // txtuser
+            // 
+            this.txtuser.Location = new System.Drawing.Point(453, 150);
+            this.txtuser.Name = "txtuser";
+            this.txtuser.Size = new System.Drawing.Size(235, 20);
+            this.txtuser.TabIndex = 45;
+            // 
+            // idfullname
+            // 
+            this.idfullname.AutoSize = true;
+            this.idfullname.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.idfullname.Location = new System.Drawing.Point(240, 348);
+            this.idfullname.Name = "idfullname";
+            this.idfullname.Size = new System.Drawing.Size(76, 18);
+            this.idfullname.TabIndex = 44;
+            this.idfullname.Text = "ชื่อ-นามสกุล";
+            // 
+            // idphone
+            // 
+            this.idphone.AutoSize = true;
+            this.idphone.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.idphone.Location = new System.Drawing.Point(238, 298);
+            this.idphone.Name = "idphone";
+            this.idphone.Size = new System.Drawing.Size(85, 18);
+            this.idphone.TabIndex = 43;
+            this.idphone.Text = "เบอร์โทรศัพท์";
+            // 
+            // idciz
+            // 
+            this.idciz.AutoSize = true;
+            this.idciz.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.idciz.Location = new System.Drawing.Point(241, 247);
+            this.idciz.Name = "idciz";
+            this.idciz.Size = new System.Drawing.Size(46, 18);
+            this.idciz.TabIndex = 42;
+            this.idciz.Text = "สถานะ";
+            // 
+            // iduser
+            // 
+            this.iduser.AutoSize = true;
+            this.iduser.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.iduser.Location = new System.Drawing.Point(241, 152);
+            this.iduser.Name = "iduser";
+            this.iduser.Size = new System.Drawing.Size(66, 18);
+            this.iduser.TabIndex = 41;
+            this.iduser.Text = "เลขสมาชิก";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(533, 89);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(285, 18);
+            this.label1.TabIndex = 40;
+            this.label1.Text = "ข้อมูลเกี่ยวกับสมาชิก : การฝากเงินสัจจะออมทรัพย์";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(451, 402);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(235, 20);
+            this.textBox1.TabIndex = 54;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(453, 484);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(235, 20);
+            this.textBox2.TabIndex = 53;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(240, 485);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 18);
+            this.label2.TabIndex = 52;
+            this.label2.Text = "รวมเป็นเงิน";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(238, 402);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(110, 18);
+            this.label3.TabIndex = 51;
+            this.label3.Text = "จำนวนเงินที่ฝากไว้";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(240, 446);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 18);
+            this.label4.TabIndex = 55;
+            this.label4.Text = "วันที่ฝาก";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(757, 486);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(32, 18);
+            this.label5.TabIndex = 57;
+            this.label5.Text = "บาท";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(757, 402);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(32, 18);
+            this.label6.TabIndex = 58;
+            this.label6.Text = "บาท";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(463, 446);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 59;
+            // 
+            // txtsel
+            // 
+            this.txtsel.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.txtsel.FormattingEnabled = true;
+            this.txtsel.Items.AddRange(new object[] {
+            "มี",
+            "ไม่มี"});
+            this.txtsel.Location = new System.Drawing.Point(451, 244);
+            this.txtsel.Name = "txtsel";
+            this.txtsel.Size = new System.Drawing.Size(235, 21);
+            this.txtsel.TabIndex = 60;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(148, 558);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(115, 30);
+            this.button1.TabIndex = 61;
+            this.button1.Text = "แก้ไข";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(361, 558);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(115, 30);
+            this.button2.TabIndex = 62;
+            this.button2.Text = "ค้นหา";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(536, 558);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(115, 30);
+            this.button4.TabIndex = 64;
+            this.button4.Text = "ลบ";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(742, 558);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(115, 30);
+            this.button5.TabIndex = 65;
+            this.button5.Text = "หน้าหลัก";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(898, 558);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(115, 30);
+            this.button6.TabIndex = 66;
+            this.button6.Text = "ย้อนกลับ";
+            this.button6.UseVisualStyleBackColor = true;
             // 
             // Usermoney1_1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1608, 919);
+            this.ClientSize = new System.Drawing.Size(1203, 739);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtsel);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtfamily);
+            this.Controls.Add(this.idfamily);
+            this.Controls.Add(this.txtphone);
+            this.Controls.Add(this.txtfullname);
+            this.Controls.Add(this.txtuser);
+            this.Controls.Add(this.idfullname);
+            this.Controls.Add(this.idphone);
+            this.Controls.Add(this.idciz);
+            this.Controls.Add(this.iduser);
+            this.Controls.Add(this.label1);
             this.Name = "Usermoney1_1";
             this.Text = "Usermoney1_1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtfamily;
+        private System.Windows.Forms.Label idfamily;
+        private System.Windows.Forms.TextBox txtphone;
+        private System.Windows.Forms.TextBox txtfullname;
+        private System.Windows.Forms.TextBox txtuser;
+        private System.Windows.Forms.Label idfullname;
+        private System.Windows.Forms.Label idphone;
+        private System.Windows.Forms.Label idciz;
+        private System.Windows.Forms.Label iduser;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.ComboBox txtsel;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
     }
 }
---
+++ finaldesktop/Usermoney1_2.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace finaldesktop
+{
+    partial class Usermoney1_2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtfamily = new System.Windows.Forms.TextBox();
+            this.idfamily = new System.Windows.Forms.Label();
+            this.txtphone = new System.Windows.Forms.TextBox();
+            this.txtfullname = new System.Windows.Forms.TextBox();
+            this.txtciz = new System.Windows.Forms.TextBox();
+            this.txtuser = new System.Windows.Forms.TextBox();
+            this.idfullname = new System.Windows.Forms.Label();
+            this.idphone = new System.Windows.Forms.Label();
+            this.idciz = new System.Windows.Forms.Label();
+            this.iduser = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtfamily
+            // 
+            this.txtfamily.Location = new System.Drawing.Point(535, 173);
+            this.txtfamily.Name = "txtfamily";
+            this.txtfamily.Size = new System.Drawing.Size(235, 20);
+            this.txtfamily.TabIndex = 50;
+            // 
+            // idfamily
+            // 
+            this.idfamily.AutoSize = true;
+            this.idfamily.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.idfamily.Location = new System.Drawing.Point(323, 173);
+            this.idfamily.Name = "idfamily";
+            this.idfamily.Size = new System.Drawing.Size(86, 18);
+            this.idfamily.TabIndex = 49;
+            this.idfamily.Text = "รหัสครอบครัว";
+            // 
+            // txtphone
+            // 
+            this.txtphone.Location = new System.Drawing.Point(533, 271);
+            this.txtphone.Name = "txtphone";
+            this.txtphone.Size = new System.Drawing.Size(235, 20);
+            this.txtphone.TabIndex = 48;
+            // 
+            // txtfullname
+            // 
+            this.txtfullname.Location = new System.Drawing.Point(535, 320);
+            this.txtfullname.Name = "txtfullname";
+            this.txtfullname.Size = new System.Drawing.Size(235, 20);
+            this.txtfullname.TabIndex = 47;
+            // 
+            // txtciz
+            // 
+            this.txtciz.Location = new System.Drawing.Point(535, 219);
+            this.txtciz.Name = "txtciz";
+            this.txtciz.Size = new System.Drawing.Size(235, 20);
+            this.txtciz.TabIndex = 46;
+            // 
+            // txtuser
+            // 
+            this.txtuser.Location = new System.Drawing.Point(535, 123);
+            this.txtuser.Name = "txtuser";
+            this.txtuser.Size = new System.Drawing.Size(235, 20);
+            this.txtuser.TabIndex = 45;
+            // 
+            // idfullname
+            // 
+            this.idfullname.AutoSize = true;
+            this.idfullname.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.idfullname.Location = new System.Drawing.Point(322, 321);
+            this.idfullname.Name = "idfullname";
+            this.idfullname.Size = new System.Drawing.Size(76, 18);
+            this.idfullname.TabIndex = 44;
+            this.idfullname.Text = "ชื่อ-นามสกุล";
+            // 
+            // idphone
+            // 
+            this.idphone.AutoSize = true;
+            this.idphone.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.idphone.Location = new System.Drawing.Point(320, 271);
+            this.idphone.Name = "idphone";
+            this.idphone.Size = new System.Drawing.Size(85, 18);
+            this.idphone.TabIndex = 43;
+            this.idphone.Text = "เบอร์โทรศัพท์";
+            // 
+            // idciz
+            // 
+            this.idciz.AutoSize = true;
+            this.idciz.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.idciz.Location = new System.Drawing.Point(323, 220);
+            this.idciz.Name = "idciz";
+            this.idciz.Size = new System.Drawing.Size(132, 18);
+            this.idciz.TabIndex = 42;
+            this.idciz.Text = "เลขประจำตัวประชาชน";
+            // 
+            // iduser
+            // 
+            this.iduser.AutoSize = true;
+            this.iduser.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.iduser.Location = new System.Drawing.Point(323, 125);
+            this.iduser.Name = "iduser";
+            this.iduser.Size = new System.Drawing.Size(66, 18);
+            this.iduser.TabIndex = 41;
+            this.iduser.Text = "เลขสมาชิก";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(655, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 18);
+            this.label1.TabIndex = 40;
+            this.label1.Text = "สมัครสมาชิก : ลูกค้า";
+            // 
+            // Usermoney1_2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1203, 739);
+            this.Controls.Add(this.txtfamily);
+            this.Controls.Add(this.idfamily);
+            this.Controls.Add(this.txtphone);
+            this.Controls.Add(this.txtfullname);
+            this.Controls.Add(this.txtciz);
+            this.Controls.Add(this.txtuser);
+            this.Controls.Add(this.idfullname);
+            this.Controls.Add(this.idphone);
+            this.Controls.Add(this.idciz);
+            this.Controls.Add(this.iduser);
+            this.Controls.Add(this.label1);
+            this.Name = "Usermoney1_2";
+            this.Text = "Usermoney1_2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtfamily;
+        private System.Windows.Forms.Label idfamily;
+        private System.Windows.Forms.TextBox txtphone;
+        private System.Windows.Forms.TextBox txtfullname;
+        private System.Windows.Forms.TextBox txtciz;
+        private System.Windows.Forms.TextBox txtuser;
+        private System.Windows.Forms.Label idfullname;
+        private System.Windows.Forms.Label idphone;
+        private System.Windows.Forms.Label idciz;
+        private System.Windows.Forms.Label iduser;
+        private System.Windows.Forms.Label label1;
+    }
+}
